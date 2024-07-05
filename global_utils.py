071d0256e2e39f4daae3c082b2a25b4c9455b4a7
Josh <joshua12.taylor12@gmail.com>
2024-07-04 23:03:36 -0600

    added new files

---
+++ global_utils.py
@@ -0,0 +1,81 @@
+from os import kill
+from pathlib import Path
+import logging
+import json
+from enum import Enum, EnumType
+
+""" Config """
+CONFIG_PATH = Path(__file__).parent / "config.json"
+
+def get_config():
+    with open(CONFIG_PATH, 'r') as f:
+        config = json.load(f)
+    return config
+config = get_config()
+
+def save_config(config: dict):
+    with open(CONFIG_PATH, "w") as f:
+        json.dump(config, f, indent=6)
+
+
+config = get_config()
+
+
+class SectionNamesDescriptor:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __get__(self, instance, owner=None):
+        return self
+
+class ImmutableMeta(type):
+    _is_initialized = False
+
+    def __new__(mcs, name, bases, class_dict):
+        user_enum_key_values = {key.upper(): value for key, value in config['section-names'].items()}
+        class_dict.update(user_enum_key_values)
+        for key, value in class_dict.items():
+            if not key.startswith("__"):
+                class_dict[key] = SectionNamesDescriptor(key, value)
+        cls = super().__new__(mcs, name, bases, class_dict)
+        cls._is_initialized = True
+        return cls
+    def is_name(cls, name:str) -> bool:
+        for attr_name in cls.__dict__:
+            if name == attr_name:
+                return True
+        return False
+    def __iter__(cls):
+        for attr_name in cls.__dict__:
+            attr_value = getattr(cls, attr_name)
+            if isinstance(attr_value, SectionNamesDescriptor):
+                yield attr_value
+
+    def __setattr__(cls, key, value):
+        if cls._is_initialized and key in cls.__dict__:
+            raise AttributeError(f"Cannot modify attribute '{key}'. SectionJ attributes are immutable.")
+        super().__setattr__(key, value)
+
+    def __iterable__(cls):
+        return cls
+
+    def __contains__(cls, value):
+        if isinstance(value, str):
+            return value in [i.value for i in cls]
+        elif isinstance(value, SectionNamesDescriptor):
+            return value.value in [i.value for i in cls]
+        else:
+            return False
+
+
+class SectionNames(metaclass=ImmutableMeta):
+    DEFINITION = "defin"
+    THEOREM = "theo"
+    DERIVATION = "der"
+    PROOF = "pf"
+    COROLLARY = "corollary"
+    LEMMA = "lemma"
+    PROPOSITION = "proposition"
+
+
---
+++ main_gui.py
@@ -0,0 +1,27 @@
+from PyQt6.QtWidgets import QApplication
+from .gui.controller import FlashcardController
+from .gui.window import MainWindow
+from .gui.flashcard_model import FlashcardModel, TexCompilationManager
+import logging
+import logging.config
+from .global_utils import get_config
+
+
+config = get_config()
+logging.config.dictConfig(config = config["flashcard-logging-config"])
+logger = logging.getLogger(__name__)
+
+def main():
+    app = QApplication([])
+    compilation_manager = TexCompilationManager()
+    flashcard_model = FlashcardModel(compilation_manager)
+    window = MainWindow()
+    controller = FlashcardController(window, flashcard_model, config)
+    window.setCloseCallback(controller.close)
+    controller.run(app)
+    if not flashcard_model.compile_thread.stopped(): # Cant remember if I actually need this
+        flashcard_model.compile_thread.wait_for_stop()
+
+if __name__ == '__main__':
+    main()
+
