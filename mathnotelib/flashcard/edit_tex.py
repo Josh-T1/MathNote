a402920cb703a9afab741cf94b2644cf085143bc
Josh <joshua12.taylor12@gmail.com>
2025-01-12 18:00:04 -0700

    changed import location to avoid requiring iTerm2

--- mathnotelib/flashcard/edit_tex.py
+++ mathnotelib/flashcard/edit_tex.py
@@ -1,4 +1,3 @@
-import iterm2
 from functools import partial
 from ..utils import config
 
@@ -13,17 +12,20 @@ def latex_template(tex: str) -> str:
 \end{{document}}"""
 
 
-async def _main(connection, filename):
-    app = await iterm2.async_get_app(connection)
-    window = app.current_window
-    if window is None: return None
-    new_window = await window.async_create(connection)
-    tab = new_window.tabs[0]
-    session = tab.sessions[0]
-
-    await new_window.async_set_frame(iterm2.Frame(iterm2.Point(500, 500), iterm2.Size(1000, 1000)))
-    await session.async_send_text(f"{config["editor"]} {filename}\n")
 
 def open_file_with_editor(filename):
+    import iterm2
+
+    async def _main(connection, filename):
+        app = await iterm2.async_get_app(connection)
+        window = app.current_window
+        if window is None: return None
+        new_window = await window.async_create(connection)
+        tab = new_window.tabs[0]
+        session = tab.sessions[0]
+
+        await new_window.async_set_frame(iterm2.Frame(iterm2.Point(500, 500), iterm2.Size(1000, 1000)))
+        await session.async_send_text(f"{config["editor"]} {filename}\n")
+
     main = partial(_main, filename=filename)
     iterm2.run_until_complete(main)
