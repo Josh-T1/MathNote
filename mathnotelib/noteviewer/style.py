870380d57d16ab0bfacae29a5d2ae2afaa827f0f
Josh <joshua12.taylor12@gmail.com>
2025-07-15 19:39:27 -0600

    refactor: progress on PyQt6 gui

---
+++ mathnotelib/noteviewer/style.py
@@ -0,0 +1,25 @@
+TOGGLE_BUTTON_CSS = """
+QPushButton {
+    border-radius: 2px;
+    background-color: #2E2E2E;
+    width: 20px;
+    height: 20px;
+    font-size: 16pt;
+}
+"""
+
+TREE_VIEW_CSS = """
+QTreeView {
+    background-color: #2E2E2E;
+    color: #d3d3d3;
+    }
+QTreeView::item:selected {
+    background-color: #444;
+    color: white;
+    }
+QTreeView::item:hover {
+    background-color: #555;
+    color: white;
+}
+"""
+
